a9a77c38830a6c65e752fdd5414e9da2eced9771
nigma_silmi <nigma.silmi@globant.com>
2024-04-08 14:16:22 -0400

    redirects

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,28 +1,37 @@
 """challenges views"""
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+
+monthly_challenges = {
+    'january': 'lorem ipsum january',
+    'february': 'lorem ipsum february',
+    'march': 'lorem ipsum march',
+    'april': 'lorem ipsum april',
+    'may': 'lorem ipsum may',
+    'june': 'lorem ipsum june',
+    'july': 'lorem ipsum july',
+    'august': 'lorem ipsum august',
+    'september': 'lorem ipsum september',
+    'october': 'lorem ipsum october',
+    'november': 'lorem ipsum november',
+    'december': 'lorem ipsum december',
+}
 
 
 def monthly_challenge_by_number(request, month):
     """View for Challenges with number"""
-    challenge_text = None
-    if month == 1:
-        challenge_text = 'january'
-    elif month == 2:
-        challenge_text = 'february'
-    else:
-        return HttpResponseNotFound()
-    return HttpResponse(challenge_text)
+    monthly_keys = list(monthly_challenges.keys())
+    target_month = monthly_keys[month - 1]
+    return HttpResponseRedirect("/challenges/" + target_month)
 
 
 def monthly_challenge(request, month):
     """View for Challenges with string"""
     challenge_text = None
-    if month == 'january':
-        challenge_text = 'january'
-    elif month == 'february':
-        challenge_text = 'february'
-    else:
-        return HttpResponseNotFound()
+    try:
+        challenge_text = monthly_challenges[month]
+    except KeyError:
+        return HttpResponseNotFound('sorry, this challenge is not available')
 
     return HttpResponse(challenge_text)
