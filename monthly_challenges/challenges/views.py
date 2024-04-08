93c545cc43eaa33c6573f61804a3871a1a8c4ea9
nigma_silmi <nigma.silmi@globant.com>
2024-04-08 13:24:37 -0400

    practicing views and routes

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -6,3 +6,8 @@ from django.http import HttpResponse
 def index(request):
     """View for index"""
     return HttpResponse("Hello world!")
+
+
+def february(request):
+    """View for February"""
+    return HttpResponse('February challenge')
