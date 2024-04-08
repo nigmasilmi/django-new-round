3345280d2965c7f5f4122a9ed938018533075b27
nigma_silmi <nigma.silmi@globant.com>
2024-04-08 16:02:46 -0400

    returning html with string interpolation

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -20,6 +20,22 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse('name-month-challenge', args=[month])
+        list_items += f'<li><a href=\"{month_path}\">{capitalized_month}</a></li>'
+
+        response_data = """
+        f'<ul>{list_items}</ul>
+
+        """
+    return HttpResponse(list_items)
+
+
 def monthly_challenge_by_number(request, month):
     """View for Challenges with number"""
     monthly_keys = list(monthly_challenges.keys())
