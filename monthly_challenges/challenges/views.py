fb8a7fbd64a1355f9b371424dfdf5f004ff69312
nigma_silmi <nigma.silmi@globant.com>
2024-04-08 13:52:15 -0400

    path converters

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,14 +3,26 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
-def monthly_challenge(request, month):
-    """View for Challenges"""
+def monthly_challenge_by_number(request, month):
+    """View for Challenges with number"""
     challenge_text = None
-    if (month == 'january'):
+    if month == 1:
         challenge_text = 'january'
-    elif (month == 'february'):
-        challenge_text = 'january'
+    elif month == 2:
+        challenge_text = 'february'
     else:
-        return HttpResponseNotFound
+        return HttpResponseNotFound()
+    return HttpResponse(challenge_text)
+
+
+def monthly_challenge(request, month):
+    """View for Challenges with string"""
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'january'
+    elif month == 'february':
+        challenge_text = 'february'
+    else:
+        return HttpResponseNotFound()
 
     return HttpResponse(challenge_text)
